794968d651d2245c8cd64ccf805c6e0d554e6623
sudhagar712 <sudhagarmsr712@gmail.com>
2025-06-18 17:26:38 +0530

    commit

--- src/Components/CategoryGrid.jsx
+++ src/Components/CategoryGrid.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-import cat2 from "../assets/images/bridewed2.mp4";
+import cat2 from "../assets/images/bridewed.mp4";
 import cat3 from "../assets/images/spanew.mp4";
 import cat4 from "../assets/images/haircut.mp4";
+import cat5 from "../assets/images/makeupvd.mp4";
+import cat6 from "../assets/images/megavd.mp4";
+
 import { Link } from "react-router-dom";
 
 const categories = [
@@ -10,6 +13,16 @@ const categories = [
     title: "Bride Makeup",
     video: cat2,
     link: "/services/bridalcollections"
+  },
+  {
+    title: "Mehandi",
+    video: cat6,
+    link: ""
+  },
+  {
+    title: "Beauty Makeup",
+    video: cat5,
+    link: "/Makeup"
   },
   {
     title: "Spa",
@@ -35,10 +48,10 @@ const CategoryGrid = () => {
         </h3>
       </div>
 
-      <div className="grid grid-cols-1 px-3 md:px-15 md:grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 px-3 md:px-[20px] md:grid-cols-3 gap-5">
         {categories.map((category, index) => (
           <Link to={category.link} key={index}>
-            <div className="relative group h-[500px] overflow-hidden cursor-pointer rounded-xl shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105">
+            <div className="relative group h-[700px] overflow-hidden cursor-pointer rounded-xl shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105">
               <video
                 src={category.video}
                 className="w-full h-full object-cover"
